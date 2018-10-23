423131c7a798949775743d223980505f9fca6744
Kinqston <vanek-cs@mail.ru>
2018-10-23 15:22:00 +1000

    First commit

---
+++ Assets/SpaceNinja/Scripts/BladeCutter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class BladeCutter : MonoBehaviour {
+    public Material CutMaterial;
+    // Use this for initialization
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject victim = collision.collider.gameObject;
+        GameObject[] parts = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, CutMaterial);
+
+        if (!parts[1].GetComponent<Rigidbody>())
+            parts[1].AddComponent<Rigidbody>();
+        Destroy(parts[1],1);
+    }
+}
---
+++ Assets/SpaceNinja/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] target_Prefab;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+    IEnumerator Spawn()
+    {
+        while (true) {
+            GameObject go = Instantiate(target_Prefab[Random.Range(0,target_Prefab.Length)]);
+            Rigidbody temp = go.GetComponent<Rigidbody>();
+            temp.velocity = new Vector3(0f,5f,5f);
+            temp.angularVelocity = new Vector3(Random.Range(-5f,5f),0f, Random.Range(-5f, 5f));
+            temp.useGravity = true;
+
+            Vector3 pos = transform.position;
+            pos.x += Random.Range(-1f, 1f);
+            go.transform.position = pos;
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
