1440cf03caa6765bedab84ccd908f9d4c2f8a6e6
Kinqston <vanek-cs@mail.ru>
2018-10-29 01:02:40 +1000

    spawn, select lvl_game

--- Assets/SpaceNinja/Scripts/GameControllerNinja.cs
+++ Assets/SpaceNinja/Scripts/GameControllerNinja.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameControllerNinja : MonoBehaviour {
+    public static byte lvl_game;
+
     public static int Score;
     public static int Score_Error;
 
     public GameObject Score_text;
     public GameObject Score_Error_text;
-
     // Use this for initialization
     void Start () {
 		
@@ -17,11 +19,19 @@ public class GameControllerNinja : MonoBehaviour {
 
     public static void GameStart()
     {
-        Spawner.SpawnMode = true;
+        Instantiate(Resources.Load("SpaceNinja/X1"));
+        Instantiate(Resources.Load("SpaceNinja/X3"));
+        //Enable_lvl_select();
+        //Spawner.SpawnMode = true;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    public static void MainMenu()
+    {
+        Instantiate(Resources.Load("SpaceNinja/start"));
+    }
+
+    // Update is called once per frame
+    void Update () {
         Score_text.GetComponent<Text>().text = Score.ToString();
         Score_Error_text.GetComponent<Text>().text = Score_Error.ToString();
     }
