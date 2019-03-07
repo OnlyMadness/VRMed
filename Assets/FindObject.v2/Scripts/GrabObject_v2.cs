747961e7cbc120af536569da14f294035b7cc3d7
ivan <vanek-cs@mail.ru>
2019-03-07 18:57:49 +1000

    findobjectsv2 betatest

--- Assets/FindObject.v2/Scripts/GrabObject_v2.cs
+++ Assets/FindObject.v2/Scripts/GrabObject_v2.cs
@@ -6,8 +6,8 @@
     public class GrabObject_v2 : VRTK_InteractableObject
     {
        // private GameObject bullet;
-        public bool Founded; 
-        
+        public bool Founded;
+        Material fsdf;
 
         public override void StartUsing(VRTK_InteractUse usingObject)
         {
@@ -17,18 +17,36 @@
             //  FireBullet();
         }
         public override void StopUsing(VRTK_InteractUse usingObject)
-        {
+        {    
             StartCoroutine(Hide());
             base.StopUsing(usingObject);
         }
         IEnumerator Hide()
         {
-            while (gameObject.GetComponent<Renderer>().material.color.a >= 0.06f)
+            //gameObject.GetComponent<Renderer>().material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);   //Opaque
+            //gameObject.GetComponent<Renderer>().material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+            //gameObject.GetComponent<Renderer>().material.SetInt("_ZWrite", 1);
+            //gameObject.GetComponent<Renderer>().material.DisableKeyword("_ALPHATEST_ON");
+            //gameObject.GetComponent<Renderer>().material.DisableKeyword("_ALPHABLEND_ON");
+            //gameObject.GetComponent<Renderer>().material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            //gameObject.GetComponent<Renderer>().material.renderQueue = -1;
+            for (int i = 0; i < gameObject.GetComponent<Renderer>().materials.Length; i++)
             {
-  
-                //gameObject.GetComponent<Rigidbody>().isKinematic = false;
-                var color = gameObject.GetComponent<Renderer>().material.color;
-                gameObject.GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, color.a-0.05f);
+                gameObject.GetComponent<Renderer>().materials[i].SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);            //Fade
+                gameObject.GetComponent<Renderer>().materials[i].SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                gameObject.GetComponent<Renderer>().materials[i].SetInt("_ZWrite", 0);
+                gameObject.GetComponent<Renderer>().materials[i].DisableKeyword("_ALPHATEST_ON");
+                gameObject.GetComponent<Renderer>().materials[i].EnableKeyword("_ALPHABLEND_ON");
+                gameObject.GetComponent<Renderer>().materials[i].DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                gameObject.GetComponent<Renderer>().materials[i].renderQueue = 3000;
+            }
+            while (gameObject.GetComponent<Renderer>().material.color.a >= 0.06f)
+            {             
+                for (int i = 0; i < gameObject.GetComponent<Renderer>().materials.Length; i++)
+                {
+                    var color = gameObject.GetComponent<Renderer>().materials[i].color;
+                    gameObject.GetComponent<Renderer>().materials[i].color = new Color(color.r, color.g, color.b, color.a - 0.05f);
+                }
                 yield return new WaitForSeconds(0.01f);
             }
             Destroy(gameObject);
